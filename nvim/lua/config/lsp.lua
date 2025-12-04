a415202ac0bbae1d3878e785a73093bda02313c4
ramory-l <ramory.lorch@icloud.com>
2025-12-04 08:58:29 +0300

    add lsp keybinding

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -26,3 +26,33 @@ vim.diagnostic.config {
     },
   },
 }
+
+-- LSP keybindings
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    -- Navigation
+    map('gd', vim.lsp.buf.definition, '[G]o to [D]efinition')
+    map('gD', vim.lsp.buf.declaration, '[G]o to [D]eclaration')
+    map('gr', vim.lsp.buf.references, '[G]o to [R]eferences')
+    map('gi', vim.lsp.buf.implementation, '[G]o to [I]mplementation')
+    map('gt', vim.lsp.buf.type_definition, '[G]o to [T]ype definition')
+
+    -- Documentation
+    map('K', vim.lsp.buf.hover, 'Hover documentation')
+    map('<C-k>', vim.lsp.buf.signature_help, 'Signature help')
+
+    -- Actions
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+    -- Diagnostics
+    map('[d', vim.diagnostic.goto_prev, 'Previous [D]iagnostic')
+    map(']d', vim.diagnostic.goto_next, 'Next [D]iagnostic')
+  end,
+})
